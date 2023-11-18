36f44e7bc950daf52776f23537e7a22c5181f56c
William Garcia <wwoodsgarcia@ufl.edu>
2023-11-17 23:08:34 -0500

    Finish writing all unit tests for Counter.test.js. The 100% coverage requirement is met

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,37 @@
 // import necessary react testing library helpers here
+import { render, fireEvent, screen } from "@testing-library/react";
 // import the Counter component here
+import Counter from "../components/Counter";
 
 beforeEach(() => {
   // Render the Counter component here
-})
-
-test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
 });
 
-test('should render initial count with value of 0', () => {
+test("renders counter message", () => {
   // Complete the unit test below based on the objective in the line above
+  const counterWord = screen.getByText(/Counter/i);
+  expect(counterWord).toBeInTheDocument();
 });
 
-test('clicking + increments the count', () => {
+test("should render initial count with value of 0", () => {
   // Complete the unit test below based on the objective in the line above
+  const initialCount = screen.getByTestId("count");
+  expect(initialCount).toHaveTextContent("0");
 });
 
-test('clicking - decrements the count', () => {
+test("clicking + increments the count", () => {
   // Complete the unit test below based on the objective in the line above
+  const incrementButton = screen.getByText("+");
+  fireEvent.click(incrementButton);
+  const countAfterIncrement = screen.getByTestId("count");
+  expect(countAfterIncrement).toHaveTextContent("1");
+});
+
+test("clicking - decrements the count", () => {
+  // Complete the unit test below based on the objective in the line above
+  const decrementButton = screen.getByText("-");
+  fireEvent.click(decrementButton);
+  const countAfterDecrement = screen.getByTestId("count");
+  expect(countAfterDecrement).toHaveTextContent("-1");
 });
